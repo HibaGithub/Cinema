1365b4bc022e3d8753dab19be966cb38f0376176
hibarhazlani <hibarhzlani@gmail.com>
2023-03-12 17:14:43 +0100

    cinemaniav0.1

---
+++ resources/js/Pages/component/header.jsx
@@ -0,0 +1,39 @@
+// <!DOCTYPE html>:
+<html lang="en">
+<head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Document</title>
+</head>
+<body>
+
+            <header>
+                <a href="#" class="logo">Cinimania</a>
+                <ul class="navigation">
+                    <li id="movies">
+                        <a href="#">Accueil </a>
+                        <div class="menu-contant">
+                            <a href="#categorie">CATEGORY</a>
+
+                            <a href="#Topmovie">TOP POPULARE MOVIES </a>
+
+                            <a href="#Newmovie">NEW MOVIES </a>
+
+                        </div>
+                    </li>
+                    <li><a href="page2.html">Horairs</a></li>
+                    <li><a href="#">Tarif</a></li>
+                </ul>
+                <div class="search">
+                    <input type="text" placeholder="search" />
+                    <i class="fa-solid fa-magnifying-glass"></i>
+                </div>
+                <a href="./cenimanialogin/index.html">
+                    <div class="login">
+                        <i id="icon-login" class="fa-solid fa-circle-user"></i>
+                    </div>
+                </a>
+            </header>
+</body>
+</html>
