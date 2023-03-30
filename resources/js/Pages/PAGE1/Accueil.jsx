e9664d0236c0323214a5020296157a9d95b0e773
ououssama <oussamaouaissa1@gmail.com>
2023-03-30 22:05:56 +0000

    clear alerts in terminal

--- resources/js/Pages/PAGE1/Accueil.jsx
+++ resources/js/Pages/PAGE1/Accueil.jsx
@@ -1,21 +1,15 @@
 import Header from "../component/header";
 import Footer from "../component/footer";
 import ContentPage from "./PageContent";
-// import FlashCard from "./flashDeals/FlashCard";
-
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "../login/log-in";
-export default function  HomePage(){
-    return(<>
-          <Header />
-          <ContentPage />
-          <Footer />
 
-        {/* <FlashCard/> */}
-        
-
-
-         </>
-    
-    )
+export default function HomePage() {
+    return (
+        <>
+            <Header />
+            <ContentPage />
+            <Footer />
+        </>
+    );
 }
